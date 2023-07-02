49b4a022a950585aabe6c2107ff98215b818ca7d
Khashem <hashemehraz@gmail.com>
2023-07-02 12:18:30 +0600

    init

---
+++ src/main/java/com/example/makersprojectbackend/MakersProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.makersprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakersProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakersProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/makersprojectbackend/MakersProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makersprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakersProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
