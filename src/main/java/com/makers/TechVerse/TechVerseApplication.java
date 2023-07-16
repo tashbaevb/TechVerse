197d36e1d53c25499923184f2b8ef9cdb4db7e89
Baitur <tawbaev777@gmail.com>
2023-07-17 01:09:14 +0600

    last one

--- src/main/java/com/makers/TechVerse/TechVerseApplication.java
+++ src/main/java/com/makers/TechVerse/TechVerseApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TechVerseApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TechVerseApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TechVerseApplication.class, args);
+    }
 }
